ebe5fbb21f9fd4e5ef961c1c9bd38dfec0769a78
corehabit <coachcorehabit@gmail.com>
2026-01-29 20:14:19 -0600

    Delete premiumStore.js

--- premiumStore.js
+++
@@ -1,9 +0,0 @@
-const premiumSessions = new Set();
-
-export function markPremium(sessionId) {
-  premiumSessions.add(sessionId);
-}
-
-export function isPremium(sessionId) {
-  return premiumSessions.has(sessionId);
-}
